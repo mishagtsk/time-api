266dd58836f7c11ea07da5f1aa640cbe25c04590
Mykhailo Hutsalenko <m.hutsalenko@playwildforest.io>
2025-05-12 20:58:51 +0300

    add yml

--- Time.Api/Time.Api/Program.cs
+++ Time.Api/Time.Api/Program.cs
@@ -16,29 +16,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/time", () => new
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        CurrentTime = DateTime.UtcNow,
+        TimeZone = "UTC"
     })
-    .WithName("GetWeatherForecast")
+    .WithName("GetTime")
     .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
